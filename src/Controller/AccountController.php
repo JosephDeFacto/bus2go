246c71d0630a95e9a069de346343bba7017db200
JosephDeFacto <josip.mareljic@gmail.com>
2023-02-26 23:40:53 +0100

    add change password

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -3,12 +3,19 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\ChangePasswordType;
 use App\Helper\ObjectIterator;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Http\Logout\LogoutUrlGenerator;
 
 /**
  * @isGranted("ROLE_USER")
@@ -47,6 +54,23 @@ class AccountController extends AbstractController
     }
 
     /**
-     * @Route("/account/change-password", name="user_change_password", methods={"GET", "POST"})
+     * @Route("/change-password", name="user_change_password", methods={"GET", "POST"})
      */
+    public function changePassword(User $user, Request $request, UserPasswordHasherInterface $hasher, EntityManagerInterface $entityManager, LogoutUrlGenerator $logoutUrlGenerator): Response
+    {
+        $form  = $this->createForm(ChangePasswordType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setPassword($hasher->hashPassword($user, $form->get('newPassword')->getData()));
+            $entityManager->flush();
+
+            return $this->redirect($logoutUrlGenerator->getLogoutPath());
+        }
+
+        return $this->render('account/change-password.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
 }
